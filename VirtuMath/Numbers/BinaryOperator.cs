19d5c85859a3afeacbf978a2636bb3c598895127
Whix100 <whix100@gmail.com>
2022-06-26 19:41:28 -0400

    Added BinaryOperator
    Added the BinaryOperator to do binary operations in PEMDAS order. Also made Literals and Constants only parse if they are alone in the input string.

---
+++ VirtuMath/Numbers/BinaryOperator.cs
@@ -0,0 +1,46 @@
+﻿namespace VirtuMath.Numbers;
+
+public class BinaryOperator : Number
+{
+    private static readonly Dictionary<string, Func<Number, Number, double>> operators =
+        new Dictionary<string, Func<Number, Number, double>>()
+        {
+            { "+", (a, b) => a.Value + b.Value },
+            { "-", (a, b) => a.Value - b.Value },
+            { "*", (a, b) => a.Value * b.Value },
+            { "/", (a, b) => a.Value / b.Value },
+            { "//", (a, b) => (int)(a.Value - b.Value) },
+            { "%", (a, b) => a.Value % b.Value },
+            { "^", (a, b) => Math.Pow(a.Value, b.Value) }
+        };
+
+    public override double Value
+        => operators[Operator].Invoke(Left, Right);
+
+    public override Number[] Children
+        => new Number[] { Left, Right };
+
+    public readonly string Operator;
+    public readonly Number Left;
+    public readonly Number Right;
+
+    public BinaryOperator(string op, Number left, Number right)
+    {
+        Operator = op;
+        Left = left;
+        Right = right;
+    }
+
+    public override string ToString(string? number_format, FormatEnum format)
+    {
+        switch (format)
+        {
+            case FormatEnum.Tags:
+                return $"<BinaryOperator Operator={Operator} Value={Value.ToString(number_format)}>" +
+                    Left.ToString(number_format, format).Replace("\n", "\n\t") + "\n" +
+                    Right.ToString(number_format, format).Replace("\n", "\n\t");
+            default:
+                return Left + Operator + Right;
+        }
+    }
+}
--- VirtuMath/Parsing/Parser.cs
+++ VirtuMath/Parsing/Parser.cs
@@ -13,18 +13,34 @@ public class Parser
     public Parser()
     {
         // language=regex
+        string ADDITION_OPERATORS = @"(\+|\-)";
+        // language=regex
+        string MULTIPLICATION_OPERATORS = @"(\*|/|//|%)";
+        // language=regex
+        string EXPONENTIAL_OPERATOR = @"(\^)";
+        // language=regex
+        string UNARY_OPERATORS = @"(\+|\-)";
+        // language=regex
         string LITERAL = @"(\d+\.?\d*)";
         // language=regex
         string CONSTANT = @"(inf|infinity|\u221E|pi|\u03C0|e|tau|\u03C4)";
         // language=regex
-        string UNARY_OPERATORS = @"(\+|\-)";
+        string OPERAND = $"{UNARY_OPERATORS}*({LITERAL}|{CONSTANT})";
 
         // language=regex
         ParsingGrammar = new Grammar(new Rule[]
         {
+            new Rule($"(?<={OPERAND})({ADDITION_OPERATORS})(?={OPERAND})", ParseBinaryOperator,
+                RegexOptions.IgnoreCase | RegexOptions.RightToLeft),
+            new Rule($"(?<=({UNARY_OPERATORS})*({LITERAL}|{CONSTANT}))({MULTIPLICATION_OPERATORS})" +
+                $"(?=({UNARY_OPERATORS})*({LITERAL}|{CONSTANT}))", ParseBinaryOperator,
+                RegexOptions.IgnoreCase | RegexOptions.RightToLeft),
+            new Rule($"(?<=({UNARY_OPERATORS})*({LITERAL}|{CONSTANT}))({EXPONENTIAL_OPERATOR})" +
+                $"(?=({UNARY_OPERATORS})*({LITERAL}|{CONSTANT}))", ParseBinaryOperator,
+                RegexOptions.IgnoreCase | RegexOptions.RightToLeft),
             new Rule($"({UNARY_OPERATORS})(?=({UNARY_OPERATORS})*({LITERAL}|{CONSTANT}))", ParseUnaryOperator),
-            new Rule(LITERAL, ParseLiteral),
-            new Rule(CONSTANT, ParseConstant)
+            new Rule($"^{LITERAL}$", ParseLiteral),
+            new Rule($"^{CONSTANT}$", ParseConstant)
         });
     }
 
@@ -36,7 +52,7 @@ public class Parser
         foreach (Rule rule in ParsingGrammar)
         {
             Match match = Regex.Match(input, rule.RegularExpression, rule.RegularOptions);
-
+            
             if (match.Success)
                 return rule.Parse.Invoke(input, match);
         }
@@ -44,12 +60,15 @@ public class Parser
         throw new FormatException("The given input is not in a recognized format");
     }
 
-    private Number ParseConstant(string input, Token match)
-        => new Constant(input);
+    private Number ParseBinaryOperator(string input, Token match)
+        => new BinaryOperator(match.Value, Parse(input[..match.Index]), Parse(input[(match.Index + match.Length)..]));
+
+    private Number ParseUnaryOperator(string input, Token match)
+        => new UnaryOperator(match.Value, Parse(input[match.Length..]));
 
     private Number ParseLiteral(string input, Token match)
         => new Literal(Convert.ToDouble(input));
 
-    private Number ParseUnaryOperator(string input, Token match)
-        => new UnaryOperator(match.Value, Parse(input[match.Length..]));
+    private Number ParseConstant(string input, Token match)
+        => new Constant(input);
 }
