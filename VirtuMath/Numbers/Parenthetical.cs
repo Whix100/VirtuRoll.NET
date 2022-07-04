3480a9e2757d51b01704e9337890608afbd807b8
Whix100 <whix100@gmail.com>
2022-07-03 23:03:01 -0400

    Added Parenthetical
    Added the Parenthetical class inheriting from Number

---
+++ VirtuMath/Numbers/Parenthetical.cs
@@ -0,0 +1,27 @@
+﻿namespace VirtuMath.Numbers;
+
+public class Parenthetical : Number
+{
+    public override double Value
+        => Inside.Value;
+
+    public override Number[] Children
+        => new Number[] { Inside };
+
+    public readonly Number Inside;
+
+    public Parenthetical(Number inside)
+        => Inside = inside;
+
+    public override string ToString(string? number_format, FormatEnum format)
+    {
+        switch (format)
+        {
+            case FormatEnum.Tags:
+                return $"<Constant>" +
+                    Inside.ToString(number_format, format).Replace("\n", "\n\t");
+            default:
+                return $"({Inside.ToString(number_format, format)})";
+        }
+    }
+}
